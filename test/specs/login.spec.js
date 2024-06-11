4256e0422198128e04b6b92e762069f455161fdf
AlionaAksani <AksaniAliona@gmail.com>
2024-06-11 22:59:28 +0300

    cleanup

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -9,30 +9,15 @@ describe('Login page', () => {
     })
 
     it('should login with valid credentials', async () => {
-        // arrange
-        //await LoginPage.open()
-        
-        // act
         await LoginPage.login('standard_user', 'secret_sauce')
 
-        // assert
         await expect(browser).toHaveUrl(expect.stringContaining(InventoryPage.path))
         await expect(InventoryPage.linkCart).toBeDisplayed();
     })
 
     it('should display error message with invalid password', async () => {
-        // arrange
-        //await LoginPage.open()
-        // User is on the login page
-
-
-        // act
         await LoginPage.login('standard_user', '12345')
-        // Enter valid login into "Login" field
-        // Enter invalid password into "Password" field
-        // Click "Login" button
 
-        // assert
         await expect(LoginPage.iconErrorUsername).toBeDisplayed()
         await expect(LoginPage.iconErrorPassword).toBeDisplayed()
         await expect(LoginPage.inputUsername).toHaveElementClass(expect.stringContaining('error'))
@@ -41,17 +26,8 @@ describe('Login page', () => {
     })
 
     it('should display error message with invalid login field', async() => {
-        //arrange
-        //await LoginPage.open()
-
-        //act
         await LoginPage.login('standarD_us', 'secret_sauce')
 
-        //Enter invalid login into "Login" field
-        //Enter valid password into "Password" field
-        //Click "Login" button
-
-        //assert
         await expect(LoginPage.iconErrorUsername).toBeDisplayed();
         await expect(LoginPage.iconErrorPassword).toBeDisplayed();
         await expect(LoginPage.inputUsername).toHaveElementClass(expect.stringContaining('error'));
